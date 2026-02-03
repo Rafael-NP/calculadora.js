bf6e0f8ce86c6693d98e5a646385ee9e7b77a3c2
Rafael <novaesrafael880@gmail.com>
2026-02-03 18:15:26 -0300

    07- fazendo as funcionalidades da calculadora

--- js/script.js
+++ js/script.js
@@ -11,3 +11,50 @@ function modo() {
         fundo.style.backgroundColor = '#fafafa'
     }
 }
+
+// NÚMEROS
+let resp = document.getElementById('resposta')
+let teclado = document.getElementById('teclado')
+let p = document.getElementsByTagName('p')[0]
+
+teclado.addEventListener('click', function (event) {
+    if (event.target.tagName !== 'BUTTON') return
+    
+    let valor = event.target.textContent
+    let ultimo = resp.textContent.slice(-1)
+    let operadores = ['+', '-', 'x', '÷']
+    let antigo = resp.textContent 
+    
+    if (valor === '⌫') {
+        resp.textContent = resp.textContent.slice(0, -1)
+        return
+    }
+    if (valor === 'C') {
+        resp.textContent = ' '
+        return
+    }
+
+    if (valor === '=') {
+        try {
+            let conta = resp.textContent
+                .replace(/x/g, '*')
+                .replace(/÷/g, '/')
+
+            resp.textContent = eval(conta)
+        } catch {
+            resp.textContent = 'Erro'
+        }
+        return
+    }
+
+    if (operadores.includes(valor) && operadores.includes(ultimo)) {
+        return
+    }
+
+    if (p.scrollWidth > resp.clientWidth) {
+            resp.textContent = antigo
+    }
+    else {
+        resp.textContent += valor
+    }
+})
